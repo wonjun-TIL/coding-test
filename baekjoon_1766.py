8e9e6a5f58ff565a6952aedf35ca531c8a4dd005
정원준 <79080825+garden-jun@users.noreply.github.com>
2024-03-25 02:08:12 +0900

    Update baekjoon_1766.py

--- baekjoon_1766.py
+++ baekjoon_1766.py
@@ -1,2 +1,26 @@
-# A+B
-## test
+N, M = map(int, input().split())
+
+graph = [[] for _ in range(N+1)]
+
+indegree = [0] * (N+1)
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+import heapq
+
+heap = []
+
+for i in range(1, N+1):
+    if indegree[i] == 0:
+        heapq.heappush(heap, i)
+
+while heap:
+    node = heapq.heappop(heap)
+    print(node, end=' ')
+    for i in graph[node]:
+        indegree[i] -= 1
+        if indegree[i] == 0:
+            heapq.heappush(heap, i)
