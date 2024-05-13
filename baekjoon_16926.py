682b77f57bcb6e909d2ee352f78f3b7220deb078
정원준 <79080825+garden-jun@users.noreply.github.com>
2024-05-14 03:48:34 +0900

    5월 14일 백준 구현 골드5, 걸린시간: 미측정

---
+++ baekjoon_16926.py
@@ -0,0 +1,21 @@
+N, M, R = map(int, input().split())
+
+matrix = [list(map(int, input().split())) for _ in range(N)]
+
+for _ in range(R):
+    for i in range(min(N, M)//2):
+        temp = matrix[i][i]
+        for j in range(i, M-i-1):
+            matrix[i][j] = matrix[i][j+1]
+        for j in range(i, N-i-1):
+            matrix[j][M-i-1] = matrix[j+1][M-i-1]
+        for j in range(M-i-1, i, -1):
+            matrix[N-i-1][j] = matrix[N-i-1][j-1]
+        for j in range(N-i-1, i, -1):
+            matrix[j][i] = matrix[j-1][i]
+        matrix[i+1][i] = temp
+
+for row in matrix:
+    print(*row)
+
+    
